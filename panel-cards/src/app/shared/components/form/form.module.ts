dfb0e8d361835deee599686977cfe0806f15922c
Genilson <geprojetosbrasil@gmail.com>
2018-08-24 19:41:35 -0300

    Estilizando o formulário de login

--- panel-cards/src/app/shared/components/form/form.module.ts
+++ panel-cards/src/app/shared/components/form/form.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormLoginComponent } from './form-login/form-login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     FormLoginComponent
