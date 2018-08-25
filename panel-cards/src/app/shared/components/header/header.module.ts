17917653824ae4b8521d8b948ed3d7013b8d359b
Genilson <geprojetosbrasil@gmail.com>
2018-08-25 16:24:52 -0300

    Criando cabeçalho da aplicação

---
+++ panel-cards/src/app/shared/components/header/header.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderNavComponent
+  ],
+  exports: [
+    HeaderNavComponent
+  ]
+})
+export class HeaderModule { }
