4f8814be6f72ba7a0996a5595b9999d443c1e650
Genilson <geprojetosbrasil@gmail.com>
2018-09-04 14:22:17 -0300

    Criando link dinâmico para visualizar detalhes do card

--- panel-cards/src/app/shared/components/card/card-list/card-list.module.ts
+++ panel-cards/src/app/shared/components/card/card-list/card-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { CardListComponent } from './card-list.component';
 import { CardBaseModule } from '../card-base/card-base.module';
@@ -14,7 +15,8 @@ import { CardNotFoundComponent } from './card-not-found/card-not-found.component
   imports: [
     CommonModule,
     CardBaseModule,
-    CardSearchModule
+    CardSearchModule,
+    RouterModule
   ],
   declarations: [
     CardListComponent,
