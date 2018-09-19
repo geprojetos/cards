b716b68016af8a0946a5dad2f627a1c23e558fb6
Genilson <geprojetosbrasil@gmail.com>
2018-09-19 11:30:53 -0300

    Criando método para a listagem de cards por páginas

--- panel-cards/src/app/shared/components/card/card-list/card-list.service.ts
+++ panel-cards/src/app/shared/components/card/card-list/card-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../../../environments/environment'
@@ -19,6 +19,17 @@ export class CardListService {
 
     return this._httpClient
       .get<CardBase[]>(api + '/' + userName + '/photos')
+  }
+
+  listCardsPagination(userName: string, page: number): Observable<CardBase[]> {
+
+    const params = new HttpParams().append('page', page.toString())
+
+    return this._httpClient
+      .get<CardBase[]>(
+        api + '/' + userName + '/photos', 
+        { params: params }
+      )
   }
 
   findCardById(id: number): Observable<CardBase[]> {
