afb2e08347331f5b44b40a73cc2ffdd3ecbbad05
Genilson <geprojetosbrasil@gmail.com>
2018-09-24 11:42:42 -0300

    Criando diretiva que verifica o owner do card

--- panel-cards/src/app/shared/directives/directives.module.ts
+++ panel-cards/src/app/shared/directives/directives.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IsLoggedDirective } from './is-logged/is-logged.directive';
+import { IsOwnerDirective } from './is-owner/is-owner.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    IsLoggedDirective
+    IsLoggedDirective,
+    IsOwnerDirective
   ],
   exports: [
-    IsLoggedDirective
+    IsLoggedDirective,
+    IsOwnerDirective
   ]
 })
 export class DirectivesModule { }
