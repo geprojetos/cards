d9e113ff20465d38276e9e14e4821f96b05660c7
Genilson <geprojetosbrasil@gmail.com>
2018-11-05 11:38:02 -0200

    Implementando o serviço do loading componente

--- panel-cards/src/app/shared/components/loading/loading-service/loading.service.ts
+++ panel-cards/src/app/shared/components/loading/loading-service/loading.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { LoadingType } from '../loading-content/loading-type';
+import { startWith } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingService {
 
+  loadingSubject = new Subject<LoadingType>();
+
   constructor() { }
+
+  getLoading() {
+
+    return this.loadingSubject
+      .asObservable()
+      .pipe(startWith(LoadingType.stopped))
+  }
+
+  start() {
+    this.loadingSubject.next(LoadingType.loading)
+  }
+
+  stop() {
+    this.loadingSubject.next(LoadingType.stopped)
+  }
 }
