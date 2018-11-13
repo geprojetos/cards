da6655b31898db2e6f6306bc7c594fa113e4f742
Genilson <geprojetosbrasil@gmail.com>
2018-11-13 18:47:10 -0200

    Criando a lógica do menu

--- panel-cards/src/app/shared/components/header/header-menu/menu-list/menu-list.component.ts
+++ panel-cards/src/app/shared/components/header/header-menu/menu-list/menu-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu-list',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuListComponent implements OnInit {
 
+  visible: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.visible = !this.visible;
   }
 
 }
